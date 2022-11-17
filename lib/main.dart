fd0e23b289d07468b01071453c8821de384de91b
theRightHooprod <60564900+theRightHooprod@users.noreply.github.com>
2022-11-17 15:23:43 -0600

    Fixed pushnamed to pop correctly

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cooking_stack/common/global_variables.dart';
 import 'package:cooking_stack/views/admin_view.dart';
 import 'package:cooking_stack/views/login.dart';
 import 'package:cooking_stack/views/main_menu.dart';
@@ -9,19 +8,45 @@ import 'package:flutter/material.dart';
 import 'common/theme_data.dart';
 import 'firebase_options.dart';
 
+Widget accountZzz(String uid) {
+  if (uid == '1OxEdNtRtkedc0aDHvhMgtyjfwx1') {
+    return MaterialApp(
+      theme: CustomTheme.theme,
+      home: const Admin(),
+    );
+  } else if (uid == 'oBtMtP8gWXZfWWJlJEzHeIvoVMx2') {
+    return MaterialApp(
+      theme: CustomTheme.theme,
+      initialRoute: '/',
+      routes: {'/': (context) => const MainMenu()},
+    );
+  } else {
+    return MaterialApp(
+      theme: CustomTheme.theme,
+      initialRoute: '/',
+      routes: {'/': (context) => const MainMenu()},
+    );
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
+//   //Do not touch
+//   FirebaseAuth.instance.authStateChanges().listen((User? user) async {
+//     runApp(MaterialApp(
+//       theme: CustomTheme.theme,
+//       home: user == null
+//           ? const LoginView()
+//           : GlobalVar.accountAkinator(user.uid),
+//     ));
+//   });
   FirebaseAuth.instance.authStateChanges().listen((User? user) async {
-    runApp(MaterialApp(
-      theme: CustomTheme.theme,
-      home: user == null
-          ? const LoginView()
-          : GlobalVar.AccountAkinator(user.uid),
-    ));
+    runApp(user == null
+        ? MaterialApp(theme: CustomTheme.theme, home: const LoginView())
+        : accountZzz(user.uid));
   });
 }
