9ae1e876f587127788f86c3c6aff04c1ee7c8f32
theRightHooprod <60564900+theRightHooprod@users.noreply.github.com>
2022-11-17 17:33:21 -0600

    Fixed cold boot

--- lib/main.dart
+++ lib/main.dart
@@ -8,45 +8,25 @@ import 'package:flutter/material.dart';
 import 'common/theme_data.dart';
 import 'firebase_options.dart';
 
-Widget accountZzz(String uid) {
-  if (uid == '1OxEdNtRtkedc0aDHvhMgtyjfwx1') {
-    return MaterialApp(
-      theme: CustomTheme.theme,
-      home: const Admin(),
-    );
-  } else if (uid == 'oBtMtP8gWXZfWWJlJEzHeIvoVMx2') {
-    return MaterialApp(
-      theme: CustomTheme.theme,
-      initialRoute: '/',
-      routes: {'/': (context) => const MainMenu()},
-    );
-  } else {
-    return MaterialApp(
-      theme: CustomTheme.theme,
-      initialRoute: '/',
-      routes: {'/': (context) => const MainMenu()},
-    );
-  }
-}
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-//   //Do not touch
-//   FirebaseAuth.instance.authStateChanges().listen((User? user) async {
-//     runApp(MaterialApp(
-//       theme: CustomTheme.theme,
-//       home: user == null
-//           ? const LoginView()
-//           : GlobalVar.accountAkinator(user.uid),
-//     ));
-//   });
+
   FirebaseAuth.instance.authStateChanges().listen((User? user) async {
-    runApp(user == null
-        ? MaterialApp(theme: CustomTheme.theme, home: const LoginView())
-        : accountZzz(user.uid));
+    runApp(MaterialApp(
+        theme: CustomTheme.theme,
+        initialRoute: user == null
+            ? 'login'
+            : user.uid == '1OxEdNtRtkedc0aDHvhMgtyjfwx1'
+                ? 'admin'
+                : '/',
+        routes: {
+          '/': (context) => const MainMenu(),
+          'admin': (context) => const Admin(),
+          'login': (context) => const LoginView()
+        }));
   });
 }
