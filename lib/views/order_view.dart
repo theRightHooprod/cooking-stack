359ea7ede7cce095650997119cb73b6ea659eb02
theRightHooprod <60564900+theRightHooprod@users.noreply.github.com>
2022-11-17 22:24:05 -0600

    Order view finished

--- lib/views/order_view.dart
+++ lib/views/order_view.dart
@@ -1,81 +1,99 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:time_elapsed/time_elapsed.dart';
 
 import '../common/global_variables.dart';
 
-class pene extends StatelessWidget {
-  const pene({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: GlobalVar.asd,
-        body: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          children: const [
-            TextField(
-              decoration: InputDecoration(hintText: 'Nombre del cliente'),
-            ),
-            SizedBox(height: 10.0),
-            TextField(
-              decoration: InputDecoration(hintText: 'Teléfono del cliente'),
-            ),
-            SizedBox(height: 10.0),
-            TextField(
-              decoration: InputDecoration(hintText: 'Teléfono del cliente'),
-            ),
-          ],
-        ));
-  }
-}
+DateTime now = DateTime.now();
+DateTime todaydate = DateTime(now.year, now.month, now.day);
+DateTime tomorrowdate = DateTime(now.year, now.month, now.day + 1);
 
 class CurrentOrders extends StatelessWidget {
   const CurrentOrders({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> ordersStream = FirebaseFirestore.instance
+        .collection('orders')
+        .where('created', isGreaterThanOrEqualTo: todaydate)
+        .where('created', isLessThan: tomorrowdate)
+        .orderBy('created', descending: true)
+        .snapshots();
+
     return Scaffold(
         appBar: GlobalVar.asd,
-        body: ListView.builder(
-            padding: const EdgeInsets.all(20),
-            itemCount: 2,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15),
-                child: Container(
-                    padding: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: GlobalVar.orange, width: 4),
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(20))),
-                    child: ListTile(
-                        trailing: Icon(
-                          Icons.access_time,
-                          color: GlobalVar.black,
-                          size: 40,
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: StreamBuilder(
+            stream: ordersStream,
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return const Center(child: Text('Algo ha salido mal 😞'));
+              }
+
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: Text("Cargando... 😽"));
+              }
+
+              if (snapshot.data!.docs.isEmpty) {
+                return const Center(
+                    child:
+                        Center(child: Text("No haz hecho ningún pedido 😞")));
+              }
+
+              return ListView(
+                children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                  Map<String, dynamic> data =
+                      document.data()! as Map<String, dynamic>;
+
+                  List<dynamic> products = data['products'] as List<dynamic>;
+
+                  String totalProductos = '';
+
+                  for (int i = 0; i < products.length; i++) {
+                    if (i < products.length - 1) {
+                      totalProductos += products[i]['name'] + ', ';
+                    } else {
+                      totalProductos += products[i]['name'];
+                    }
+                  }
+
+                  if (totalProductos.length > 20) {
+                    totalProductos = '${totalProductos.substring(0, 20)}...';
+                  }
+
+                  DateTime date = data['created'].toDate();
+
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: ListTile(
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Cliente: ${data['clientname']}'),
+                            Text(
+                                'Estado: ${data['satus'] == null ? 'En preparación ${TimeElapsed.fromDateTime(date)}' : data['satus'] == 1 ? 'Finalizado' : 'Cancelado'}'),
+                          ],
                         ),
-                        subtitle: RichText(
-                            text: TextSpan(children: [
-                          TextSpan(
-                              text: 'Orden: ',
-                              style: TextStyle(
-                                  color: GlobalVar.black, fontSize: 20)),
-                          const TextSpan(
-                              text: '#123456789',
-                              style:
-                                  TextStyle(color: Colors.green, fontSize: 20)),
-                          const TextSpan(text: '\n'),
-                          TextSpan(
-                              text: 'Hora: ',
-                              style: TextStyle(
-                                  color: GlobalVar.black, fontSize: 20)),
-                          const TextSpan(
-                              text: '17:00',
-                              style:
-                                  TextStyle(color: Colors.green, fontSize: 20)),
-                        ])))),
+                        leading: Icon(
+                          data['satus'] == null
+                              ? Icons.access_time
+                              : data['satus'] == 1
+                                  ? Icons.done
+                                  : Icons.cancel_outlined,
+                          color: GlobalVar.black,
+                          size: 50,
+                        ),
+                        subtitle: Text('Productos: $totalProductos'),
+                      ),
+                    ),
+                  );
+                }).toList(),
               );
-            }));
+            },
+          ),
+        ));
   }
 }
